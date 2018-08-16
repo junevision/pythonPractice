5553f13b752f1e3743de80d9464c78e1fd738e6f
junevision <1012762794@qq.com>
2018-08-16 18:40:28 +0800

    20180816update

---
+++ day2/集合.py
@@ -0,0 +1,10 @@
+# coding: utf-8
+# Author: Mingjun Lei
+
+# 集合：无序、去重
+
+list_1 = [1, 4, 5, 7, 3, 6, 7, 9]  # 需去重
+list_1 = set(list_1)  # 设为集合
+
+list_2 = set([66, 22, 4, 6, 88])
+print(list_1, list_2)
