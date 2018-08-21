8d05ff16fb7cb665fabe9ddf3942c013dc6a403a
junevision <1012762794@qq.com>
2018-08-21 17:37:55 +0800

    20180821update

--- Wechat_Moments_Sudoku/sudoku.py
+++ Wechat_Moments_Sudoku/sudoku.py
@@ -2,16 +2,15 @@
 # Author: Mingjun Lei
 
 from PIL import Image
-import sys
 
 # 先将 input image 填充为正方形
 def fill_image(image):
     width, height = image.size
-    # 选取长和宽中较大值作为新图片的
+    # 选取长和宽中较大值作为新图片的边长
     new_image_length = width if width > height else height
-    # 生成新图片[白底]
+    # 生成新图片[白底]，底色可配置其他颜色
     new_image = Image.new(image.mode, (new_image_length, new_image_length), color='white')
-    # 将之前的图粘贴在新图上，居中
+    # 将之前的图片input image 粘贴在新图上，居中
     if width > height:  # 原图宽大于高，则填充图片的竖直维度  #(x,y)二元组表示粘贴上图相对下图的起始位置,是个坐标点。
         new_image.paste(image, (0, int((new_image_length - height) / 2)))
     else:
@@ -19,7 +18,7 @@ def fill_image(image):
     return new_image
 def cut_image(image):
     width, height = image.size
-    item_width = int(width / 3)   # 因为朋友圈一行放3张图。
+    item_width = int(width / 3)   # 因为朋友圈一行放3张图
     box_list = []
     # (left, upper, right, lower)
     for i in range(0, 3):
@@ -29,16 +28,15 @@ def cut_image(image):
             box_list.append(box)
     image_list = [image.crop(box) for box in box_list]
     return image_list
-#保存
+# 保存
 def save_images(image_list):
     index = 1
     for image in image_list:
         image.save(str(index) + '.png', 'PNG')
         index += 1
 if __name__ == '__main__':
-    file_path = "1.jpg"
+    file_path = "1.jpg"  # 把目标图片input image 放到代码所处的文件夹里
     image = Image.open(file_path)
-    #image.show()
     image = fill_image(image)
     image_list = cut_image(image)
     save_images(image_list)
