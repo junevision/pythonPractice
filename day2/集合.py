8224fe27bbefdc77558160301120793e35019d84
JUN <1012762794@qq.com>
2018-09-12 01:07:17 +0800

    [201809120107]update

--- day2/集合.py
+++ day2/集合.py
@@ -27,3 +27,29 @@ print(list_1.issuperset(list_3))  # list_1是list_3的父集
 
 list_4 = set([2, 8])
 print(list_1.isdisjoint(list_4))  # 如果两个集合没有交集，则返回true
+
+print("-------------------------------")
+# 交集
+print(list_1 & list_2)
+# 并集 union
+print(list_2 | list_1)
+# 差集 difference
+print(list_1 - list_2)  # in list_1 but not in list_2
+# 对称差集
+print(list_1 ^ list_2)
+
+list_1.add(999)  # 无序添加一项
+print(list_1)
+list_1.update([888, 777, 666])  # 无序添加多项
+print(list_1)
+list_1.remove(7)  # 删除一项（天生去重，不存在有重复的值）
+print(list_1)
+print(len(list_1))  # 集合的长度
+
+print(list_1.pop())  # 随机删除一个集合的元素
+print(list_1.pop())
+print(list_1.pop())
+print(list_1.pop())
+
+list_1.discard(999)  # 删除；删除一个不是集合的元素返回none，不会报错
+print(list_1)
