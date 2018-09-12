21e2dfcaf5d045425b404f471485ef03a918dc5b
junevision <1012762794@qq.com>
2018-09-12 18:44:07 +0800

    [201809121844]update

---
+++ day2/file_op.py
@@ -0,0 +1,6 @@
+# coding: utf-8
+# Author: Mingjun Lei
+
+# data = open("yesterday", encoding="utf-8").read()  # 由于Windows系统默认是gbk编码打开，所以要转换为python的编码打开
+f = open("yesterday", encoding="utf-8")  # 文件句柄
+
