ab49dca6af58b348eecd156f91e6466efeb8bc70
junevision <1012762794@qq.com>
2018-12-18 18:58:27 +0800

    [201812181858] update

--- day2/encode2.py
+++ day2/encode2.py
@@ -6,4 +6,5 @@ Author = "Mingjun Lei"
 s = "你好呀"
 print(s.encode("gbk"))
 print(s.encode("utf-8"))
-print(s.encode("utf-8").decode("utf-8").encode("gb2312"))
+print(s)
+print(s.encode("utf-8").decode("utf-8").encode("gb2312").decode("gb2312"))
